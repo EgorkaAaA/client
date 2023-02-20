5ad63b2110dbe0b0d08a2e0c6c2474d710324332
eilebedev <eilebedev@phoenixit.ru>
2023-02-20 13:47:35 +0300

    remove scope

--- src/main/java/ru/lebedev/sber/client/config/Config.java
+++ src/main/java/ru/lebedev/sber/client/config/Config.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 public class Config {
 
 	@Bean
-	@Scope(scopeName = "prototype")
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
